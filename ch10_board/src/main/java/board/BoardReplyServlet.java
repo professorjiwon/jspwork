67ba29315d4d5f6508ec715dda8f85c010e15be4
professorjiwon <professorjiwon@gmail.com>
2024-07-24 17:23:32 +0900

    07.24_7교시

--- ch10_board/src/main/java/board/BoardReplyServlet.java
+++ ch10_board/src/main/java/board/BoardReplyServlet.java
@@ -11,6 +11,7 @@ public class BoardReplyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		Board board = new Board();
 		board.setName(request.getParameter("name"));
 		board.setSubject(request.getParameter("subject"));
@@ -21,8 +22,10 @@ public class BoardReplyServlet extends HttpServlet {
 		board.setDepth(Integer.parseInt(request.getParameter("depth")));
 		board.setIp(request.getParameter("ip"));
 		
+		BoardDao bDao = new BoardDao();
+		bDao.replyPosUpdate(board.getRef(), board.getPos());
+		bDao.replyBoard(board);
 		
-		
+		response.sendRedirect("list.jsp?nowPage=" + request.getParameter("nowPage"));
 	}
-
 }
